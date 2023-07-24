17fc3f86d772966d6b70a3ec734beef4a8e2caab
Gautier404 <and.jgautier@gmail.com>
2023-07-23 20:01:52 -0700

    process ingredients and consolidate them

--- ralph/kroger.py
+++ ralph/kroger.py
@@ -1,5 +1,8 @@
 def add_item_to_cart():
     return
 
-def get_item_price():
-    return
+def get_item_price(UPC, kroger_token: str) -> float:
+    return 1.0
+
+def get_item_size(UPC, kroger_token: str) -> str:
+    return "1 potato"
